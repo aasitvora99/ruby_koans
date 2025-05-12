17b52fbd3fdbfd410c9ce5881b070ae55bec514f
Aasit Vora <aasit@browserstack.com>
2025-05-12 18:23:57 +0530

    chore: i m stupid

--- koans/about_arrays.rb
+++ koans/about_arrays.rb
@@ -29,18 +29,18 @@ class AboutArrays < Neo::Koan
     assert_equal :jelly, array[3]
     assert_equal :jelly, array.last
     assert_equal :jelly, array[-1]
-    assert_equal :peanut, array[-3]
+    assert_equal :butter, array[-3]
   end
 
   def test_slicing_arrays
     array = [:peanut, :butter, :and, :jelly]
 
-    assert_equal [:peanut, :butter], array[0,1]
-    assert_equal [:peanut, :butter, :and] , array[0,2]
-    assert_equal [:and], array[2,2]
+    assert_equal [:peanut], array[0,1]
+    assert_equal [:peanut, :butter] , array[0,2]
+    assert_equal [:and, :jelly], array[2,2]
     assert_equal [:and, :jelly], array[2,20]
-    assert_equal nil, array[4,0]
-    assert_equal nil, array[4,100]
+    assert_equal [], array[4,0]
+    assert_equal [], array[4,100]
     assert_equal nil, array[5,0]
   end
 
